dacecc37effab5427092d7e48da33e85dacfb76e
Rob Donoghue <rdonoghue@gmail.com>
2022-05-29 22:01:26 -0400

    so far so good

--- modalwindow/readme.md
+++ modalwindow/readme.md
@@ -1,3 +1,5 @@
 # Modal Window Project
 
 Again, just using the readme for notes
+
+- querySelectorAll will get ALL items of a particular class and put them in an array in order
--- modalwindow/script.js
+++ modalwindow/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelector('.show-modal');
+const btnOpenModalAll = document.querySelectorAll('.show-modal');
+
+console.log(btnOpenModal);
+console.log(btnOpenModalAll);
+
+for (let i = 0; i < btnOpenModalAll.length; i++) {
+  console.log(btnOpenModalAll[i].textContent);
+  btnOpenModalAll[i].addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
+btnCloseModal.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
+
+overlay.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
