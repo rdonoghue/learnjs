7818a9932d7eaa155ad1f29d8d7f0ae905efd0e5
Rob Donoghue <rdonoghue@gmail.com>
2022-05-30 20:18:17 -0400

    Played around with changing multiple incidences of a class simultaneously

--- modalwindow/readme.md
+++ modalwindow/readme.md
@@ -3,3 +3,4 @@
 Again, just using the readme for notes
 
 - querySelectorAll will get ALL items of a particular class and put them in an array in order
+- Ah, ok, this seems liek the first real use for 'const functionname = function(){foo}', because then you can just call functionname when it comes up.
--- modalwindow/script.js
+++ modalwindow/script.js
@@ -1,34 +1,76 @@
 'use strict';
 
 const modal = document.querySelector('.modal');
+const allModals = document.querySelectorAll('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelector('.show-modal');
 const btnOpenModalAll = document.querySelectorAll('.show-modal');
+const testers = document.querySelector('.toformat');
+const allTesters = document.querySelectorAll('.toformat');
+const btnChange = document.querySelector('.change-format');
 
-console.log(btnOpenModal);
-console.log(btnOpenModalAll);
+let flip = 'pink';
+
+const closeModal = function () {
+  for (let i = 0; i < allModals.length; i++) {
+    allModals[i].classList.add('hidden');
+  }
+  overlay.classList.add('hidden');
+};
+// Trying silly stuff here
+
+btnChange.addEventListener('click', function () {
+  console.log('Click Confirmed');
+  for (let i = 0; i < allTesters.length; i++) {
+    // console.log(allTesters[i].classList);
+    if (flip == 'pink') {
+      allTesters[i].classList.remove('tester1');
+      allTesters[i].classList.add('tester2');
+    } else {
+      allTesters[i].classList.remove('tester2');
+      allTesters[i].classList.add('tester1');
+    }
+  }
+  if (flip == 'pink') {
+    flip = 'blue';
+  } else {
+    flip = 'pink';
+  }
+});
+
+// function echo(evalue, iter = 0) {
+//   // testers.evalue.classlist.remove('tester');
+//   // testers.evalue.classlist.add('tester2');
+//   evalue[iter].classlist.remove('tester');
+//   console.log('---');
+//   console.log(evalue);
+//   console.log(iter);
+//   iter = iter + 1;
+// }
+
+// const foobar = ['foo', 'bar', 'baz'];
+// allTesters.forEach(echo);
+
+console.log(allTesters);
+
+// more real stuff
 
 for (let i = 0; i < btnOpenModalAll.length; i++) {
   console.log(btnOpenModalAll[i].textContent);
   btnOpenModalAll[i].addEventListener('click', function () {
-    modal.classList.remove('hidden');
+    allModals[i].classList.remove('hidden');
     overlay.classList.remove('hidden');
   });
 }
-btnCloseModal.addEventListener('click', function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-});
-
-overlay.addEventListener('click', function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-});
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
 
 document.querySelector('body').addEventListener('keypress', function (event) {
   if (event.code === 'Escape') {
-    modal.classList.add('hidden');
+    for (let i = 0; i < allModals.length; i++) {
+      allModals[i].classList.add('hidden');
+    }
     overlay.classList.add('hidden');
   }
 });
