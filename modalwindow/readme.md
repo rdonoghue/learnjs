51ed4f66b203442c3b1e9966fc0fe9581b252d1d
Rob Donoghue <rdonoghue@gmail.com>
2022-05-31 20:33:12 -0400

    Started the Pig project.

--- modalwindow/readme.md
+++ modalwindow/readme.md
@@ -4,3 +4,4 @@ Again, just using the readme for notes
 
 - querySelectorAll will get ALL items of a particular class and put them in an array in order
 - Ah, ok, this seems liek the first real use for 'const functionname = function(){foo}', because then you can just call functionname when it comes up.
+- Made my own life harder with this one by adding multiple modals but still tryign tohandle it all as arrays rather than just building each modal. Code is a little uglier too, because when I do something like hit escape, it doesn't necessarily know which modal is open without my either A) running a check or B) keeping the open modal in a variable. I went with A, because B seemed clunky, but I dunno if that was the right call.
--- modalwindow/script.js
+++ modalwindow/script.js
@@ -4,6 +4,7 @@ const modal = document.querySelector('.modal');
 const allModals = document.querySelectorAll('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
+const btnAllClose = document.querySelectorAll('.close-modal');
 const btnOpenModal = document.querySelector('.show-modal');
 const btnOpenModalAll = document.querySelectorAll('.show-modal');
 const testers = document.querySelector('.toformat');
@@ -14,9 +15,13 @@ let flip = 'pink';
 
 const closeModal = function () {
   for (let i = 0; i < allModals.length; i++) {
-    allModals[i].classList.add('hidden');
+    if (!allModals[i].classList.contains('hidden')) {
+      allModals[i].classList.add('hidden');
+    }
   }
-  overlay.classList.add('hidden');
+  if (!overlay.classList.contains('hidden')) {
+    overlay.classList.add('hidden');
+  }
 };
 // Trying silly stuff here
 
@@ -63,14 +68,43 @@ for (let i = 0; i < btnOpenModalAll.length; i++) {
     overlay.classList.remove('hidden');
   });
 }
+
+for (let i = 0; i < btnAllClose.length; i++) {
+  console.log(btnAllClose[i].textContent);
+  btnAllClose[i].addEventListener('click', function () {
+    if (!allModals[i].classList.contains('hidden')) {
+      allModals[i].classList.add('hidden');
+    }
+    if (!overlay.classList.contains('hidden')) {
+      overlay.classList.add('hidden');
+    }
+  });
+}
+
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
-document.querySelector('body').addEventListener('keypress', function (event) {
-  if (event.code === 'Escape') {
-    for (let i = 0; i < allModals.length; i++) {
-      allModals[i].classList.add('hidden');
+// document.querySelector('body').addEventListener('keypress', function (event) {
+//   if (event.code === 'Escape') {
+//     for (let i = 0; i < allModals.length; i++) {
+//       allModals[i].classList.add('hidden');
+//     }
+//     overlay.classList.add('hidden');
+//   }
+// });
+
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+  if (e.code === 'Escape') {
+    for (let i = 0; i < btnOpenModalAll.length; i++) {
+      if (!allModals[i].classList.contains('hidden')) {
+        console.log('Checkign if hidden');
+        closeModal();
+        // allModals[i].classList.add('hidden');
+        // overlay.classList.add('hidden');
+      }
     }
-    overlay.classList.add('hidden');
   }
 });
+
+// I definitely made my own life harder with this one my insisting on useing the array of elements for the modals.  if I'd declared each one individually, the code would be MUCH tidier.
